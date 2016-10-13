e12b42520d507c25d1274f2ed1a44085b81e3e6a
Inconsolable523 <1273258990@qq.com>
2016-10-13 23:36:30 +0800

    完善

--- 静态页面3/js/main.js
+++ 静态页面3/js/main.js
@@ -9,7 +9,7 @@ require.config({
     }
 });
 
-define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookies,API) {
+define(['jquery', 'js.cookie', 'url', 'jquery.md5', 'Juicer'], function ($, Cookies, API) {
     var index = {
         init: function () {
             index.topbarControl.init();
@@ -54,8 +54,8 @@ define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookie
 
             },
             //取消关注
-            _remove: function(){
-                $("#J_remove").bind("click",function(){
+            _remove: function () {
+                $("#J_remove").bind("click", function () {
                     $('#J_nav-care').show();
                     $('#J_followed').hide();
                     var fans = parseInt($("#J_fans span").html());
@@ -85,7 +85,7 @@ define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookie
                             setTimeout(function () {
                                 $("#J_login").hide();
                                 $("#J_background").hide();
-                            },1000);
+                            }, 1000);
 
                             self._followed();
                         }
@@ -97,8 +97,8 @@ define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookie
                 self._close();
             },
             _followed: function () {
-                $.get(API.changeFollowState,function(b){
-                    if(b==1){
+                $.get(API.changeFollowState, function (b) {
+                    if (b == 1) {
                         //设置关注成功cookie
                         Cookies.set('followSuc', 'true', {expires: 7, path: '/'});
                         $('#J_nav-care').hide();
@@ -114,8 +114,8 @@ define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookie
 
 
             },
-            _close: function(){
-                $("#J_close").bind("click",function(){
+            _close: function () {
+                $("#J_close").bind("click", function () {
                     $("#J_login").hide();
                     $("#J_background").hide();
                 })
@@ -171,171 +171,193 @@ define(['jquery', 'js.cookie','url', 'jquery.md5','Juicer'], function ($, Cookie
         //    autoplay();
         //}
         //新轮播
-        lunbo:function() {
-            var $img = $("#J_banner img");
+        lunbo: function () {
+            var $img = $("#J_banner li");
             var $btn = $("#J_btn span");
+            var $banner = $(".header-banner");
             var b_index = 1;
             var timer = null;
             //自动播放
             _autoplay();
             //鼠标悬停,暂停播放
-            $img.bind("mouseover",function(){
+            $banner.bind("mouseover", function () {
                 clearInterval(timer);
-            })
+            });
+            $banner.bind("mouseout", function () {
+                _autoplay();
+            });
+
             //点击按钮,实现相应图片跳转
-            $btn.bind("click",function(){
-                var getNumber=$(this).attr("J_index");
+            $btn.on("click", function () {
+                var getNumber = $(this).attr("J_index");
                 _turnBanner(getNumber);
-            })
+            });
             //自动播放
-            function _autoplay(){
-                timer=setInterval(function(){
+            function _autoplay() {
+                timer = setInterval(function () {
                     _turnBanner(b_index);
                     b_index++;
-                    if(b_index>2){
-                        b_index=0;
+                    if (b_index > 2) {
+                        b_index = 0;
                     }
-                },5000)
+                }, 3000)
             }
-            function _turnBanner(index){
+
+            function _turnBanner(index) {
                 $img.fadeOut();
-                $img.eq(index).fadeIn(500);
+                $img.eq(index).fadeIn();
                 $btn.removeClass('on');
                 $btn.eq(index).addClass('on');
             }
         },
         //左侧内容区切换
-        contentSwitch:function(){
-                var $btn1=$("#J_change1");
-                var $btn2=$("#J_change2");
-                var dtd= $.Deferred();
-                index._getCorseCard(1, 20, $btn1.attr('data-type'), dtd);
+        contentSwitch: function () {
+            var $btn1 = $("#J_change1");
+            var $btn2 = $("#J_change2");
+            var dtd = $.Deferred();
+            index._getCorseCard(1, 20, $btn1.attr('data-type'), dtd);
 
-                $btn1.bind("click",function(){
-                    //var dtd= $.Deferred(); //新建异步对象
-                    $.when(index._getCorseCard(1, 20, $btn1.attr('data-type'), dtd)).done(function(){
-                        $btn2.removeClass("check");
-                        $btn1.addClass("check");
-                    })
-                });
-                $btn2.bind("click",function(){
-                    //var dtd= $.Deferred();
-                    $.when(index._getCorseCard(1, 20, $btn2.attr('data-type'), dtd)).done(function(){
-                        $btn1.removeClass("check");
-                        $btn2.addClass("check");
-                    })
+            $btn1.bind("click", function () {
+                //var dtd= $.Deferred(); //新建异步对象
+                $.when(index._getCorseCard(1, 20, $btn1.attr('data-type'), dtd)).done(function () {
+                    $btn2.removeClass("check");
+                    $btn1.addClass("check");
                 })
-            },
-            /**
-             * 获取课程卡片
-             * @param pageNo    页码
-             * @param psize     每页个数
-             * @param type      分类
-             * @param dtd       异步对象
-             */
-            _getCorseCard:function(pageNo, psize, type, dtd){
-                var param = {
+            });
+            $btn2.bind("click", function () {
+                //var dtd= $.Deferred();
+                $.when(index._getCorseCard(1, 20, $btn2.attr('data-type'), dtd)).done(function () {
+                    $btn1.removeClass("check");
+                    $btn2.addClass("check");
+                })
+            })
+        },
+        /**
+         * 获取课程卡片
+         * @param pageNo    页码
+         * @param psize     每页个数
+         * @param type      分类
+         * @param dtd       异步对象
+         */
+        _getCorseCard: function (pageNo, psize, type, dtd) {
+            var param = {
                     pageNo: pageNo,
                     psize: psize,
                     type: type
                 },
-                    $showPicture=$("#J_main-picture");
-                $showPicture.html("内容加载中");
-                $.get(API.getCoures,param,function(rsp){
-                    var tpldata={};//新建对象
-                    rsp=JSON.parse(rsp);//JSON.parse()将字符串解析为JSON对象
-                    tpldata.list=rsp.list;
+                $showPicture = $("#J_main-picture");
+            $showPicture.html("内容加载中");
+            $.get(API.getCoures, param, function (rsp) {
+                var tpldata = {};//新建对象
+                rsp = JSON.parse(rsp);//JSON.parse()将字符串解析为JSON对象
+                tpldata.list = rsp.list;
 
 
-                    $showPicture.html("");
-                    var cont= juicer($("#courseTpl").html(),tpldata);
-                    $showPicture.html(cont);
-                    index._page(rsp.pagination);//分页
+                $showPicture.html("");
+                var cont = juicer($("#courseTpl").html(), tpldata);
+                $showPicture.html(cont);
+                index._page(rsp.pagination);//分页
 
-                    dtd.resolve();//改变dtd的执行状态(已完成状态),进而执行done()回调函数
-                });
+                dtd.resolve();//改变dtd的执行状态(已完成状态),进而执行done()回调函数
+            });
 
-                return dtd;
-            },
+            return dtd;
+        },
         //分页
-        _page:function(data){
+        _page: function (data) {
             //页面数字渲染
             draw();
-            var $prv=$(".prv");
-            var $next=$(".next");
-            var $listNum=$(".list-number");
+            var $prv = $(".prv");
+            var $next = $(".next");
+            var $listNum = $(".list-number");
 
-            $listNum.bind("click",function(){
+            $listNum.bind("click", function () {
                 turnTopage($(this).html());
             })
-            $prv.bind("click",function(){
+            $prv.bind("click", function () {
                 $(".list-number.list-checked").prev().trigger("click");
             })
-            $next.bind("click",function(){
+            $next.bind("click", function () {
                 $(".list-number.list-checked").next().trigger("click");
             })
 
             //页面数字渲染
-            function draw(){
-                var $list=$("#J_classList"),
+            function draw() {
+                var $list = $("#J_classList"),
                     tpl;
 
                 $list.html('');
-                for(var i=1;i<=data.totlePageCount;i++){
-                    tpl=$list.html()+'\<a href="#" class="list-number">'+i+'\</a>';
+                for (var i = 1; i <= data.totlePageCount; i++) {
+                    tpl = $list.html() + '\<a href="#" class="list-number">' + i + '\</a>';
                     $list.html(tpl);
                 }
                 $list.html(tpl);
-                var $listBtn=$(".list-number");
+                var $listBtn = $(".list-number");
                 $listBtn.removeClass('list-checked');
                 $listBtn.eq(data.pageIndex).addClass('list-checked');
 
-            };
+            }
             //跳到相应页面
-            function turnTopage(pageNum){
-                var type=$('#J_leftChange a.check').attr('data-type');
-                index._getCorseCard(pageNum,data.pageSize,type);
+            function turnTopage(pageNum) {
+                var type = $('#J_leftChange a.check').attr('data-type');
+                index._getCorseCard(pageNum, data.pageSize, type);
             }
 
         },
         //右侧视频介绍
-        playVideo: function(){
-            $("#J_play").bind("click",function(){
+        playVideo: function () {
+            $("#J_play").bind("click", function () {
                 $("#J_background").show();
                 $("#J_showVideo").show();
                 reMovePlay();
                 //取消播放
-                function reMovePlay(){
-                   $("#J_reMovePlay").bind("click",function(){
-                       $("#J_showVideo").hide();
-                       $("#J_background").hide();
-                   })
+                function reMovePlay() {
+                    $("#J_reMovePlay").bind("click", function () {
+                        $("#J_showVideo").hide();
+                        $("#J_background").hide();
+                    })
                 }
             })
         },
         //右侧热门推荐
-        hotList:function(){
-            $.get(API.hotList,function(rsp){
-                var tpl={},
-                $hot=$("#J_hot");
+        hotList: function () {
+            $.get(API.hotList, function (rsp) {
+                var tpl = {},
+                    $hot = $("#J_hot");
 
-                rsp=JSON.parse(rsp);
-                tpl.list=rsp;
+                rsp = JSON.parse(rsp);
+                tpl.list = rsp;
 
                 $hot.html('');
-                var data=juicer($("#hotRank").html(),tpl);
+                var data = juicer($("#hotRank").html(), tpl);
+                var timer=null;
+                var i=0;
                 $hot.html(data);
                 scrol();
-                //轮播滚动(有问题)
-                function scrol(){
-                    var $test=$("#J_hotRank");
-                   var timer =setInterval(function(){
-                       $test.hide();
-                       $test = $test.next();
-                       $test.hide();
-                    },5000);
-
+                //轮播滚动
+                function scrol() {
+                    var $test = $(".J_hotRank");
+                    console.log($test.length);
+                        timer = setInterval(function () {
+                        $test.eq(i).hide();
+                        i++;
+                        if(i>=10){
+                            for(var j=0;j<10;j++){
+                                $test.eq(j).show();
+                            }
+                            i=0;
+                        }
+                    }, 5000);
                 }
+                //鼠标移入停止播放
+                $("#J_hot").bind("mouseover",function(){
+                    clearInterval(timer);
+                });
+                $("#J_hot").bind("mouseout",function(){
+
+                    scrol();
+                });
+
             });
 
 
